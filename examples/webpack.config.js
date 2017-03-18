85e4fcac82524c85f59eb7f9b2dee69b257e03e2
Daniel Król <daniel@krol.me>
2017-03-18 08:21:18 +0100

    Update dependencies, switch to webpack2

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,25 +1,29 @@
+var webpack = require('webpack');
 var path = require('path');
 
-module.exports = {
+const isProduction = process.env.NODE_ENV === 'production';
+
+var config = {
     resolve: {
         // .js is required for react imports.
         // .tsx is for our app entry point.
         // .ts is optional, in case you will be importing any regular ts files.
-        extensions: ['', '.js', '.ts', '.tsx']
+        extensions: ['.js', '.ts', '.tsx']
     },
     module: {
-        preLoaders: [
+        rules: [
             {
                 test: /\.tsx?$/,
-                loader: "tslint"
+                loader: "tslint-loader",
+                enforce: "pre"
+            },
+            {
+                // Set up ts-loader for .ts/.tsx files and exclude any imports from node_modules.
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
             }
-        ],
-        loaders: [{
-            // Set up ts-loader for .ts/.tsx files and exclude any imports from node_modules.
-            test: /\.tsx?$/,
-            loader: 'ts-loader',
-            exclude: /node_modules/
-        }]
+        ]
     },
     context: path.resolve(__dirname),
     entry: {
@@ -31,5 +35,30 @@ module.exports = {
     },
     devServer: {
         contentBase: path.resolve(__dirname, "public_html")
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+            }
+        })
+    ]
 };
+
+if (!isProduction) {
+    config.plugins.unshift(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin()
+    );
+    config.devServer.hot = true;
+} else {
+    config.plugins.push(
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    );
+}
+
+module.exports = config;
